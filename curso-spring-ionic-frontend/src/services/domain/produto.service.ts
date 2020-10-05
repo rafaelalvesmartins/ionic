f69ed8f7fea1e60f277e5f0037b4642ac211f921
rafaelalvesmartins <rafaelmartinsalves@gmail.com>
2020-10-04 22:34:54 -0300

    Carregando produtos de uma dada categoria

---
+++ curso-spring-ionic-frontend/src/services/domain/produto.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { API_CONFIG } from "../../config/api.config";
+
+
+@Injectable()
+export class ProdutoService{
+    constructor(public http: HttpClient){
+
+    }
+
+    findBycategoria(categoria_id:string){
+        return  this.http.get(`${API_CONFIG.baseUrl}/produtos?categorias=${categoria_id}`);
+    }
+}
